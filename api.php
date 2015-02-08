855b0039d9d31ba44a861a8c7eafbc97b6690577
NullPatrol <demon@ub3rmail.com>
2015-02-08 21:23:05 +0100

    Hotfix
    Fixed key because vnstat was too lazy to do so.

--- api.php
+++ api.php
@@ -1,23 +1,23 @@
 <?PHP
 #New Improved API System
 
-if(!isset($_GET['user'])){ $user = crypt(time()); }
-	else{ $user = escapeshellcmd($_GET['user']); }
-	    $key = escapeshellcmd($_GET['key']);
-		$host = escapeshellcmd($_GET['host']);
-		$port = escapeshellcmd($_GET['port']);
-		$time = escapeshellcmd($_GET['time']);
-		$method = escapeshellcmd($_GET['method']);
-		$key = "1234";
-	echo "Info Set<br>";
-
-    
-if($method == "stop"){
-	echo exec('perl client.pl "'.$_GET['user'].' stop'.'"');
-	echo "Stopped";
-} else{
-	echo exec('perl client.pl "'.$_GET['user'].' '.$_GET['method'].' '.$_GET['host'].' '.$_GET['port'].' '.$_GET['time'].'"');
-	echo "Sent; Ran ''";
+if(!isset($_GET['user'])) { $user = crypt(time()); } else{
+  $user = escapeshellcmd($_GET['user']); }
+  $keyy = escapeshellcmd($_GET['key']);
+  $host = escapeshellcmd($_GET['host']);
+  $port = escapeshellcmd($_GET['port']);
+  $time = escapeshellcmd($_GET['time']);
+  $method = escapeshellcmd($_GET['method']);
+  $key = "1234";
+  if ($keyy == $key) {
+    echo "Info Set<br>";
+    if($method == "stop"){
+      echo exec("perl client.pl {$_GET['user']} stop");
+      echo "Stopped";
+    } else{
+      echo exec("perl client.pl {$_GET['user']} {$_GET['method']} {$_GET['host']} {$_GET['port']} {$_GET['time']}");
+      echo "Sent; Ran ''";
+    }
+  } else {
 }
-
 ?>
